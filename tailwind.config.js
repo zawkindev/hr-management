47594d99379a0586ae7dcc599a4c9c68d195e020
zawkindev <zawkindev@gmail.com>
2024-01-02 03:49:48 +0500

    feat: create card, input and button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+    content: [
+        './pages/**/*.{html,js}',
+        './components/**/*.{html,js}',
+    ],
 }
-
